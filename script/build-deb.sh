a3591a5a14511a16f075c4cd058937d20f4b677d
xulang <xulang@uniontech.com>
2025-08-07 14:10:39 +0800

    fix deb package dependency

--- script/build-deb.sh
+++ script/build-deb.sh
@@ -232,7 +232,7 @@ Package: ${PROJECT_NAME}
 Version: ${VERSION}
 Architecture: ${ARCH}
 Maintainer: DKapture Team <dkapture@example.com>
-Depends: libbpf-dev, libuuid1
+Depends: libbpf1, libuuid1
 Priority: optional
 Section: utils
 Description: Deepin Kernel Capture - eBPF-based system observation tools
